70cad4c54a67bc5cf3a763c9eb3e0bca5e675f50
aaron0609ac <58867893+aaron0609ac@users.noreply.github.com>
2025-08-13 14:22:42 -0600

    Cambios Login
    Cambios estructura de login a la api

--- local/auth.php
+++ local/auth.php
@@ -5,18 +5,18 @@ if (!class_exists('EncrypData_')) {
 require 'db.php';
 
 function authenticate($conn) {
-
+    $secretKey = "ClaveFibretaDe32OptacteresSPL";
     $headers = getallheaders();
     $email = isset($headers['X-Email']) ? $headers['X-Email'] : null;
-    $password = isset($headers['X-Password']) ? $headers['X-Password'] : null;
-    $cardcode = isset($headers['X-Cardcode']) ? $headers['X-Cardcode'] : null;
+    $cardcode = isset($headers['X-CardCode']) ? $headers['X-CardCode'] : null;
+    $apiKey = isset($headers['X-ApiKey']) ? $headers['X-ApiKey'] : null;
 
-    if (!$email || !$password) {
+    if (!$email || !$apiKey || !$cardcode) {
         http_response_code(401);
         echo json_encode(["error" => "Faltan credenciales"]);
         exit;
     }
-    $sql = "SELECT password FROM login_cliente WHERE cardcode = '".$cardcode."' AND email = '".$email."'";
+    $sql = "SELECT password,ApiKey FROM login_cliente WHERE cardcode = '".$cardcode."' AND email = '".$email."'";
     $result = $conn->query($sql);
     if ($result->num_rows === 0) {
         http_response_code(403);
@@ -24,15 +24,31 @@ function authenticate($conn) {
         exit;
     }else{
         if ($row = $result->fetch_assoc()) {
-            $EncrypData = new EncrypData_('password');
-            $passwordDencryp = $EncrypData->cadenaDecrypt($row['password']); 
-           if($passwordDencryp!=$password){
-            http_response_code(403);
-            echo json_encode(["error" => "Credenciales incorrectas"]);
-            exit;
-           }
+            if ($row['ApiKey']==='') {
+                http_response_code(403);
+                echo json_encode(["error" => "Usuario invalido"]);
+                exit;
+            }else{
+                if(!leerApiKey($apiKey, $secretKey)){
+                    http_response_code(403);
+                        echo json_encode(["error" => "API KEY incorrectas"]);
+                }else{
+                    if($apiKey!=$row['ApiKey']){
+                        http_response_code(403);
+                        echo json_encode(["error" => "Credenciales incorrectas"]);
+                        exit;
+                    }
+                }
+            }
         }
     }
    
+}
+
+function leerApiKey($apiKey, $secretKey) {
+    $decoded = base64_decode($apiKey);
+    $iv = substr($decoded, 0, 16);
+    $encryptedData = substr($decoded, 16);
+    return openssl_decrypt($encryptedData, 'AES-256-CBC', $secretKey, 0, $iv);
 }
 ?>
